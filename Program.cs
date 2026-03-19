02460f4d8cbd204984714530d04d6f1ad0f993fd
mohas7im <mohammedhashim530@gmail.com>
2026-03-19 12:22:51 +0530

    simple calculator

--- Program.cs
+++ Program.cs
@@ -1,28 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// // See https://aka.ms/new-console-template for more information
+
+// using System;
+
+// class Dog
+// {
+//     public string Name;
+//     public string Breed;
+
+//     public void Bark()
+//     {
+//         Console.WriteLine(Name + " says woooofff");
+
+
+// }
+// }
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Dog myDog = new Dog();
+//         myDog.Name = "Bruno";
+//         myDog.Breed = "German";
+
+//         myDog.Bark();
+//     }
+// }
 
 using System;
 
-class Dog
+
+Calculator calc = new Calculator();
+Console.WriteLine(calc.Add(5, 3));      
+Console.WriteLine(calc.Multiply(4, 6));  
+
+
+class Calculator
 {
-    public string Name;
-    public string Breed;
-
-    public void Bark()
+    public int Add(int a, int b)
     {
-        Console.WriteLine(Name + " says woooofff");
+        return a + b;
+    }
 
-
-}
-}
-class Program
-{
-    static void Main(string[] args)
+    public int Multiply(int a, int b)
     {
-        Dog myDog = new Dog();
-        myDog.Name = "Bruno";
-        myDog.Breed = "German";
-
-        myDog.Bark();
+        return a * b;
     }
 }
-

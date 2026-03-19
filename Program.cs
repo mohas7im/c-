f0fc13c46589fe7c620738bd429fc4e2ee0b9403
mohas7im <mohammedhashim530@gmail.com>
2026-03-19 15:52:29 +0530

    inheritance

--- Program.cs
+++ Program.cs
@@ -26,23 +26,126 @@
 //     }
 // }
 
-using System;
+// using System;
 
 
-Calculator calc = new Calculator();
-Console.WriteLine(calc.Add(5, 3));      
-Console.WriteLine(calc.Multiply(4, 6));  
+// Calculator calc = new Calculator();
+// Console.WriteLine(calc.Add(5, 3));      
+// Console.WriteLine(calc.Multiply(4, 6));  
 
 
-class Calculator
+// class Calculator
+// {
+//     public int Add(int a, int b)
+//     {
+//         return a + b;
+//     }
+
+//     public int Multiply(int a, int b)
+//     {
+//         return a * b;
+//     }
+// }
+
+// BankAccount acc = new BankAccount("Priya", 15000);
+// acc.ShowBalance();
+// class BankAccount
+// {
+//     public string Owner;
+//     public double Balance;
+
+//     public BankAccount(string owner, double balance)
+//     {
+//         Owner = owner;
+//         Balance = balance;
+//     }
+
+//     public void ShowBalance()
+//     {
+//         Console.WriteLine(Owner + "'s balance " + Balance);
+
+//     }
+// }
+
+
+// Student s = new Student();
+// s.Age = 20;
+// s.Age = -5;
+// Console.WriteLine(s.Age);
+
+// class Student
+// {
+//     private int age;
+//     public int Age
+
+//     {
+//         get { return age; }
+//         set
+//         {
+//             if (value >= 0)
+//                 age = value;
+//             else
+//                 Console.WriteLine("Invalid age!");
+
+//         }
+//     }
+
+
+// }
+
+// Employee e = new Employee();
+// e.Salary = 50000;
+// e.ShowSalary();
+// e.Salary = -100;
+
+
+// class Employee
+// {
+//     private double salary;
+//     public double Salary
+//     {
+//         get
+//         {
+//             return salary;
+//         }
+//         set
+//         {
+//             if (value > 0)
+//             salary = value;
+
+//             else
+//             Console.WriteLine("salary must be positive");
+
+//         }
+//     }
+//     public void ShowSalary()
+//     {
+//         Console.WriteLine("Employee salary"+salary);
+//     }
+// }
+
+Dog d = new Dog();
+d.Name = "rex";
+d.Eat();
+d.Bark();
+
+
+class Animal
 {
-    public int Add(int a, int b)
+    public string Name;
+    public void Eat()
     {
-        return a + b;
+            Console.WriteLine(Name + "is eating ");     
     }
 
-    public int Multiply(int a, int b)
+}
+class Dog : Animal
+{
+    public void Bark()
     {
-        return a * b;
+        Console.WriteLine(Name + "says : woof !");
+
+
     }
 }
+

d7b64c32f3ebb44a87714db985e1d43b4b9ffdc9
mohas7im <mohammedhashim530@gmail.com>
2026-03-19 12:03:00 +0530

    class and object

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Dog
+{
+    public string Name;
+    public string Breed;
+
+    public void Bark()
+    {
+        Console.WriteLine(Name + " says woooofff");
+
+
+}
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog myDog = new Dog();
+        myDog.Name = "Bruno";
+        myDog.Breed = "German";
+
+        myDog.Bark();
+    }
+}
+
